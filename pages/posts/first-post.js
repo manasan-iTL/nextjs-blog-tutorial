85074ca659f20c47eab329a213de1c0de10e859c
manasan-iTL <daiyaofssh2020@gmail.com>
2022-06-29 14:40:10 +0900

    [change] CSSの適用

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,12 @@
 import Head from "next/head"
 import Link from "next/link"
 import Script from 'next/script'
+import Layout from '../../components/layout'
+
 
 const FirstPost = () => {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -21,7 +23,7 @@ const FirstPost = () => {
                     <a>Back to home</a>
                 </Link> 
             </h2>
-        </>
+        </Layout>
     )
 }
 
